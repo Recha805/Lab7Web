61521ea460ae521b6f394c3c4a4e197c53e4409b
Reza Irfan <81579730+Recha805@users.noreply.github.com>
2021-05-11 10:00:28 +0700

    Add files via upload

---
+++ php_dasar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP Dasar</title>
+</head>
+<body>
+<h1>Belajar PHP Dasar</h1>
+<?php
+echo "Hello World";
+?>
+<h2>Menggunakan variable</h2>
+<?php
+$nim = "31191057";
+$nama = 'Recha Irfan Trijianto';
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama";
+?>
+</body>
+</html>
